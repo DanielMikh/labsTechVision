15b09af28d704c4a65a7cf542e0409609d0bf15e
Danil Mikhailenko <danmm-1998@mail.ru>
2020-03-03 20:19:36 +0300

    ЛР4

--- lab4_2.py
+++ lab4_2.py
@@ -1,12 +1,16 @@
 from cv2 import cv2
+''' 
+    Применить на практике бинаризацию всех типов для изображений.
+    Подобрано по 2 примера изображений на каждый тип, иллюстрирующие разницу результатов использованных методов.
+'''
 
-"""Для дорог лучше подходит метод TRESH_TOZERO, т.к. картинка монотонна и полученное изображение четчё при ручном подборе трешхолда"""
+"""Для дорог лучше подходит метод TRESH_TOZERO, т.к. картинка монотонна, а полученное изображение четчё при ручном подборе трешхолда"""
 img = cv2.imread('Doroga1.png', cv2.IMREAD_GRAYSCALE)
 
 treshold, img1 = cv2.threshold(img, thresh = 200, maxval = 255, type = cv2.THRESH_TOZERO)
 treshold, img2 = cv2.threshold(img, 200, 255, cv2.THRESH_OTSU)
 img3 = cv2.adaptiveThreshold(img, maxValue = 255, adaptiveMethod = cv2.ADAPTIVE_THRESH_MEAN_C, 
-                            treshholdType = cv2.THRESH_BINARY, blockSize = 11, C = -4)
+thresholdType = cv2.THRESH_BINARY, blockSize = 11, C = -4)
 
 
 img_2 = cv2.imread('Doroga2.png', cv2.IMREAD_GRAYSCALE)
