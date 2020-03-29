cf351ffb9d07a0f3c3d327007250de312bb945c6
Danil Mikhailenko <danmm-1998@mail.ru>
2020-03-30 00:37:15 +0300

    UPD: while for infinity calculator

--- Calculator.py
+++ Calculator.py
@@ -1,52 +1,63 @@
 from math import pi, e, floor
 
 # Калькулятор
+answer = True
+while answer:
+    x, y = input('Введите первое и второе число через пробел: ').split()
+    # y = input('Введите второе:')
 
-x, y = input('Введите первое и второе число через пробел:').split()
-# y = input('Введите второе:')
+    if x == 'pi':
+        x = pi
+    elif x == 'e':
+        x = e
 
-if x == 'pi':
-    x = pi
-elif x == 'e':
-    x = e
+    if y == 'pi':
+        y = pi
+    elif y == 'e':
+        y = e
 
-if y == 'pi':
-    y = pi
-elif y == 'e':
-    y = e
+    a = float(x)
+    b = float(y)
 
-a = float(x)
-b = float(y)
+    c = input('+, -, /, *, mod, pow, div, floor: ')
+    if c == '+':
+        print(a + b)
+    elif c == '-':
+        print(a - b)
+    elif c == '/':
+        try:
+            print(a / b)
+        except ZeroDivisionError:
+            print('ТЫ ДЕЛИФ НА НОЛЬ')
+        # if b == 0:
+        #     print('Деление на 0!')
+        # else:
+        #     print(a / b)
+    elif c == '*':
+        print(a * b)
+    elif c == 'mod':
+        if b == 0:
+            print('Деление на 0!')
+        else:
+            print(a % b)
+    elif c == 'pow':
+        print(a**b)
+    elif c == 'div':
+        if b == 0:
+            print('Деление на 0!')
+        else:
+            print(a//b)
+    elif c == 'floor':
+        print('a:',floor(a))
+        print('b:', floor(b))
+    else: print('Нет такой операции')
 
-c = input('+, -, /, *, mod, pow, div, floor: ')
-if c == '+':
-    print(a + b)
-elif c == '-':
-    print(a - b)
-elif c == '/':
-    try:
-        print(a / b)
-    except ZeroDivisionError:
-        print('ТЫ ДЕЛИФ НА НОЛЬ')
-    # if b == 0:
-    #     print('Деление на 0!')
-    # else:
-    #     print(a / b)
-elif c == '*':
-    print(a * b)
-elif c == 'mod':
-    if b == 0:
-        print('Деление на 0!')
+    answer = input('Хотите продолжить: Да/Нет? ')
+    answer = answer.lower()
+
+    if answer == 'да':
+        answer = True
     else:
-        print(a % b)
-elif c == 'pow':
-    print(a**b)
-elif c == 'div':
-    if b == 0:
-        print('Деление на 0!')
-    else:
-        print(a//b)
-elif c == 'floor':
-    print('a:',floor(a))
-    print('b:', floor(b))
-else: print('Нет такой операции')
+        answer = False
+
+
